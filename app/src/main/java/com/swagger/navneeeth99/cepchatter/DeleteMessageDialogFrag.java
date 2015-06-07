6850876d5ca26b5b8ef521a8b7d2f10c8f6dc41c
DudeSurfer <navneeth.k50@gmail.com>
2015-06-07 19:09:40 +0800

    Catch parse error when deleting message
    should work now, haven’t tested

--- app/src/main/java/com/swagger/navneeeth99/cepchatter/DeleteMessageDialogFrag.java
+++ app/src/main/java/com/swagger/navneeeth99/cepchatter/DeleteMessageDialogFrag.java
@@ -5,8 +5,10 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -22,15 +24,19 @@ public class DeleteMessageDialogFrag extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("Are you sure you wish to delete " + getArguments().getString("friendName") + "?")
+        builder.setMessage("Are you sure you wish to delete " + getArguments().getString("msgTitle") + "?")
                 .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        ParseQuery<ParseUser> query = ParseUser.getQuery();
-                        query.whereEqualTo("objectId", getArguments().getString("friendID"));
-                        query.findInBackground(new FindCallback<ParseUser>() {
+                        ParseQuery<PMessage> query = new ParseQuery<>("PMessage");
+                        query.whereEqualTo("objectId", getArguments().getString("msgID"));
+                        query.getFirstInBackground(new GetCallback<PMessage>() {
                             @Override
-                            public void done(List<ParseUser> parseUsers, ParseException e) {
-                                ParseUser.getCurrentUser().removeAll("friends", parseUsers);
+                            public void done(PMessage pMessage, ParseException e) {
+                                try {
+                                    pMessage.delete();
+                                } catch (ParseException error) {
+                                    Log.d("error", error.toString());
+                                }
                             }
                         });
                     }
