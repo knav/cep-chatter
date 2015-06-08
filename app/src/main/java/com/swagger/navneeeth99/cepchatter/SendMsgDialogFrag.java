5b00d0e70e1f4f5b276744feca62264afdc2899f
ChampBenRockz <seraphi123@yahoo.com.sg>
2015-06-08 19:13:09 +0800

    Tested some weird shit

--- app/src/main/java/com/swagger/navneeeth99/cepchatter/SendMsgDialogFrag.java
+++ app/src/main/java/com/swagger/navneeeth99/cepchatter/SendMsgDialogFrag.java
@@ -58,7 +58,7 @@ public class SendMsgDialogFrag extends DialogFragment {
         LayoutInflater mLayoutInflater = getActivity().getLayoutInflater();
         mLL = (LinearLayout)mLayoutInflater.inflate(R.layout.fragment_send_message, null);
 
-        final ImageButton mUploadImageButton = (ImageButton)mLL.findViewById(R.id.uploadImageButton);
+        final ImageButton mUploadImageButton = (ImageButton)mLL.findViewById(R.id.sendNewImageButton);
         final EditText mMessageET = (EditText)mLL.findViewById(R.id.newMsgET);
         final EditText mMessageTitleET = (EditText)mLL.findViewById(R.id.newMsgTitleET);
         final Spinner mFriendSpinner = (Spinner)mLL.findViewById(R.id.friendSpinner);
@@ -80,10 +80,10 @@ public class SendMsgDialogFrag extends DialogFragment {
                     @Override
                     public void done(ParseUser parseUser, ParseException e) {
                         Log.d("check", parseUser.getUsername());
-                        int spinnerPostion = mCurrFriendsArray.indexOf(parseUser);
-                        Log.d("check", String.valueOf(spinnerPostion));
-                        mFriendSpinner.setSelection(spinnerPostion);
-                        spinnerPostion = 0;
+                        int spinnerPosition = mCurrFriendsArray.indexOf(parseUser);
+                        Log.d("check", String.valueOf(spinnerPosition));
+                        mFriendSpinner.setSelection(spinnerPosition);
+                        spinnerPosition = 0;
                     }
                 });
             }
@@ -202,7 +202,7 @@ public class SendMsgDialogFrag extends DialogFragment {
                 image = stream.toByteArray();
                 file = new ParseFile("new_image.jpeg", image);
                 file.saveInBackground();
-                ((ImageButton)mLL.findViewById(R.id.uploadImageButton)).setImageBitmap(bitmap);
+                ((ImageButton)mLL.findViewById(R.id.sendNewImageButton)).setImageBitmap(bitmap);
             } catch (FileNotFoundException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
